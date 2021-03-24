4ae50e4e7433135286769823e49a9bca3b9b0bf1
Tucker Hoog <tuckerhoog@tutanota.com>
2021-03-23 21:04:22 -0400

    95% of the way there

--- test/tests.test.js
+++ test/tests.test.js
@@ -5,18 +5,17 @@ import { countsAsAYes } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a no', (expect) => {
+test('time to test a yes', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = false;
+    const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = countsAsAYes('no');
+    const actual = countsAsAYes('yes');
 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected, 'yes returns true'); 
     
 });
-
---
+++ utils.js
@@ -0,0 +1,3 @@
+export function countsAsAYes (input) {
+    return (input[0] === 'y' || input[0] === 'Y');
+}
